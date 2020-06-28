86d8b4e05b3402dedae8d48bf28ab85c48ba62d5
Ilyas Garipov <ilyasgaripov97@gmail.com>
2020-06-28 10:28:17 +0300

    Fetch data from input fields and update products amounts

--- main.js
+++ main.js
@@ -1,2 +1,30 @@
+let milkAmount = 0
+let cheeseAmount = 0
 
+const updateProductAmount = (input, productName) => {
+    if (input.className === productName) {
+        return input.value;
+    }
+}
 
+const updateProductsAmounts = (input) => {
+    cheeseAmount = updateProductAmount(input,"cheese");
+    milkAmount = updateProductAmount(input, "milk")
+}
+
+const fetchProductsAmounts = () => {
+    const container = document.querySelector('.ui-controllers');
+
+    const productSections = Array.from(container.children);
+    productSections.forEach(section => {
+
+        section.addEventListener('keyup', () => {
+            const input = section.querySelector('input');
+            updateProductsAmounts(input);
+
+            // use values of input fields for rendering a products on table
+        })
+    })
+}
+
+fetchProductsAmounts();
