dd8ff4ee14ff1ee71680d71c07db14d6f4d3a750
Ilyas Garipov <ilyasgaripov97@gmail.com>
2020-06-28 11:30:53 +0300

    Add keyup and increase/decrease buttons event listeners for products

--- main.js
+++ main.js
@@ -12,18 +12,39 @@ const updateProductsAmounts = (input) => {
     milkAmount = updateProductAmount(input, "milk")
 }
 
+
+function keyupEventListener(section) {
+    section.addEventListener('keyup', () => {
+        const input = section.querySelector('input');
+        updateProductsAmounts(input);
+        // use values of input fields for rendering a products on table
+    })
+}
+
+function buttonClickEventListener(section) {
+    section.addEventListener('click', e => {
+        const input = section.querySelector('input');
+
+        if (e.target.className === 'increase') {
+            ++input.value;
+            updateProductsAmounts(input);
+        }
+        if (e.target.className === 'decrease') {
+            --input.value;
+            updateProductsAmounts(input);
+        }
+    })
+}
+
 const fetchProductsAmounts = () => {
     const container = document.querySelector('.ui-controllers');
 
     const productSections = Array.from(container.children);
     productSections.forEach(section => {
 
-        section.addEventListener('keyup', () => {
-            const input = section.querySelector('input');
-            updateProductsAmounts(input);
+        keyupEventListener(section);
+        buttonClickEventListener(section);
 
-            // use values of input fields for rendering a products on table
-        })
     })
 }
 
